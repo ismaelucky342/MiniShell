3fe2a2e551f866842a37c7a8a2f6e1d9d741dd45
ismaelucky342 <ismaelhc342@gmail.com>
2024-09-06 16:15:55 +0200

    minor ast updates

--- src/ast_tree/ast_process.c
+++ src/ast_tree/ast_process.c
@@ -6,27 +6,27 @@
 /*   By: ismherna <ismherna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 13:03:16 by ismherna          #+#    #+#             */
-/*   Updated: 2024/09/06 13:04:36 by ismherna         ###   ########.fr       */
+/*   Updated: 2024/09/06 16:04:50 by ismherna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	process_call_next(t_astb *tool, t_ast_node **sep)
+int	process_call_next(t_astb *tool, t_ast_node **sep, t_mem_context *ctx)
 {
 	if (ft_token_issep(tool->current_token))
 	{
-		*sep = ft_node_new(SEPARATOR);
+		*sep = ft_node_new(SEPARATOR, ctx);
 		if (*sep == NULL)
 			return (ERROR);
 		insert_node(tool, *sep);
-		if (process(tool) == ERROR)
+		if (process(tool, ctx) == ERROR)
 			return (ERROR);
 	}
 	return (SUCCESS);
 }
 
-int	process(t_astb *tool)
+int	process(t_astb *tool, t_mem_context *ctx)
 {
 	t_ast_node	*cmd;
 	t_ast_node	*sep;
@@ -37,7 +37,7 @@ int	process(t_astb *tool)
 	if (ft_token_issep(tool->current_token)
 		|| ft_token_iseot(tool->current_token))
 		return (ft_tree_build_error(tool, BAD_TOKEN));
-	cmd = ft_node_new(CMD);
+	cmd = ft_node_new(CMD, ctx);
 	if (cmd == NULL)
 		return (ERROR);
 	while (ft_token_isword(tool->current_token)
@@ -47,5 +47,5 @@ int	process(t_astb *tool)
 			continue ;
 	}
 	insert_node(tool, cmd);
-	return (process_call_next(tool, &sep));
+	return (process_call_next(tool, &sep, ctx));
 }
