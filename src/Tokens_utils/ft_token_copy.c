430513ed465cb4c8967b1a430e519b6529776778
ismaelucky342 <ismaelhc342@gmail.com>
2024-09-06 10:32:27 +0200

    tokens except ascii dic

--- src/Tokens_utils/ft_token_copy.c
+++ src/Tokens_utils/ft_token_copy.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_token_copy.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ismherna <ismherna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/06 10:29:55 by ismherna          #+#    #+#             */
+/*   Updated: 2024/09/06 10:31:11 by ismherna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/minishell.h"
 
-t_token			*ft_token_copy(t_token *token)
+t_token	*ft_token_copy(t_token *token)
 {
-	t_token		*new;
+	t_token	*new;
 
 	new = mmalloc(sizeof(t_token));
 	new->type = token->type;
--- src/Tokens_utils/ft_token_parenthesis.c
+++ src/Tokens_utils/ft_token_parenthesis.c
@@ -1,6 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_token_parenthesis.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ismherna <ismherna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/06 10:28:48 by ismherna          #+#    #+#             */
+/*   Updated: 2024/09/06 10:29:27 by ismherna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/minishell.h"
 
-int				ft_token_isparenthesis(t_token *token)
+int	ft_token_isparenthesis(t_token *token)
 {
-	return (ft_token_istype(token, CHAR_TYPE_LEFT_PARENTHESIS | CHAR_TYPE_RIGHT_PARENTHESIS));
+	return (ft_token_istype(token, CHAR_TYPE_LEFT_PARENTHESIS
+			| CHAR_TYPE_RIGHT_PARENTHESIS));
 }
