9f246a5595180c8161f9877ebbb33e974d860e56
ismaelucky342 <ismaelhc342@gmail.com>
2024-11-30 11:25:04 +0100

    aux pwd, backup env and leaks

--- src/env/enviroment.c
+++ src/env/enviroment.c
@@ -6,7 +6,7 @@
 /*   By: apollo <apollo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 14:42:45 by ismherna          #+#    #+#             */
-/*   Updated: 2024/11/22 14:14:13 by apollo           ###   ########.fr       */
+/*   Updated: 2024/11/30 11:24:12 by apollo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -146,4 +146,78 @@ int	ft_env_build(t_minishell *boogeyman, char *key_val)
 		ft_new_env(boogeyman, key_val);
 	free(key);
 	return (OK);
+}
+/**
+* @brief Checks if PWD is present in the environment and creates a backup if not.
+*
+* This function verifies if the PWD variable is present in the environment.
+* If it is not present, it creates a backup of the current working directory
+* using getcwd() and adds it to the environment.
+*
+* @param boogeyman Pointer to the `t_minishell` structure containing the
+* state of the minishell.
+* @return int Returns `OK` if the operation was successful, `KO` on error.
+*/
+int	ft_check_and_backup_pwd(t_minishell *boogeyman)
+{
+	char	*pwd;
+	char	*backup_pwd;
+	int		exists;
+
+	get_value_from_env(boogeyman->envp, "PWD", &exists);
+	if (!exists)
+	{
+		pwd = getcwd(NULL, 0);
+		if (!pwd)
+			return (KO);
+		backup_pwd = ft_strjoin("PWD=", pwd);
+		free(pwd);
+		if (!backup_pwd)
+			return (KO);
+		if (ft_new_env(boogeyman, backup_pwd) == KO)
+		{
+			free(backup_pwd);
+			return (KO);
+		}
+		free(backup_pwd);
+	}
+	return (OK);
+}
+
+/**
+* @brief Removes the backup PWD from the environment if it exists.
+*
+* This function checks if the backup PWD variable is present in the environment
+* and removes it if it exists.
+*
+* @param boogeyman Pointer to the `t_minishell` structure containing the
+* state of the minishell.
+*/
+void	ft_remove_backup_pwd(t_minishell *boogeyman)
+{
+	char	**current;
+	char	*key = "PWD";
+	int		exists;
+
+	get_value_from_env(boogeyman->envp, key, &exists);
+	if (exists)
+	{
+		current = boogeyman->envp;
+		while (*current)
+		{
+			if (!ft_strncmp(*current, key, ft_strlen(key)))
+			{
+				free(*current);
+				while (*(current + 1))
+				{
+					*current = *(current + 1);
+					current++;
+				}
+				*current = NULL;
+				boogeyman->env_elems--;
+				break ;
+			}
+			current++;
+		}
+	}
 }
